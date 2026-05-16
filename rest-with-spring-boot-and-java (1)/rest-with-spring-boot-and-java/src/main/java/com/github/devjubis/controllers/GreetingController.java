75b7e823fa282ae338c9b622c0c6e2b0f5079858
devjubis <castro.ju837@gmail.com>
2026-05-15 23:40:29 -0300

    First Steps in Java with Spring Boot 3.4.0

---
+++ rest-with-spring-boot-and-java (1)/rest-with-spring-boot-and-java/src/main/java/com/github/devjubis/controllers/GreetingController.java
@@ -0,0 +1,25 @@
+package com.github.devjubis.controllers;
+
+import com.github.devjubis.model.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+
+
+    //http:localhost:8080/greeting?name=Juliana
+    @RequestMapping("/greeting")
+    public Greeting greeting(
+            @RequestParam(value = "name", defaultValue = "Word")
+            String name){
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+ }
---
+++ rest-with-spring-boot-and-java (1)/rest-with-spring-boot-and-java/src/main/java/com/github/devjubis/model/Greeting.java
@@ -0,0 +1,5 @@
+package com.github.devjubis.model;
+
+public record Greeting(long id, String content ) {
+
+}
